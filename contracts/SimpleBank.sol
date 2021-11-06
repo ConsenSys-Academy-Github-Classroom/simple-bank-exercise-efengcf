95528da86d5339d8263484b67014bb65ee8e7bc0
efengcf <evan@coinfund.io>
2021-11-05 22:38:59 -0400

    completed w tests passing

--- contracts/SimpleBank.sol
+++ contracts/SimpleBank.sol
@@ -32,11 +32,11 @@ contract SimpleBank {
     event LogEnrolled(address accountAddress);
 
     // Add 2 arguments for this event, an accountAddress and an amount
-    event LogDepositMade();
+    event LogDepositMade(address accountAddress, uint amount);
 
     // Create an event called LogWithdrawal
     // Hint: it should take 3 arguments: an accountAddress, withdrawAmount and a newBalance 
-    event LogWithdrawal();
+    event LogWithdrawal(address accountAddress, uint withdrawAmount, uint newBalance);
 
     /* Functions
      */
@@ -56,6 +56,7 @@ contract SimpleBank {
       // 1. A SPECIAL KEYWORD prevents function from editing state variables;
       //    allows function to run locally/off blockchain
       // 2. Get the balance of the sender of this transaction
+      return balances[msg.sender];
     }
 
     /// @notice Enroll a customer with the bank
@@ -63,22 +64,25 @@ contract SimpleBank {
     // Emit the appropriate event
     function enroll() public returns (bool){
       // 1. enroll of the sender of this transaction
-      enrolled[msg.sender] = true;  
+      enrolled[msg.sender] = true;
+      return enrolled[msg.sender];
+      emit LogEnrolled(msg.sender);
     }
 
     /// @notice Deposit ether into bank
     /// @return The balance of the user after the deposit is made
-    function deposit() public returns (uint) {
+    function deposit() public payable returns (uint) {
       // 1. Add the appropriate keyword so that this function can receive ether
     
       // 2. Users should be enrolled before they can make deposits
-
+        require (enrolled[msg.sender]== true, "User is not currently enrolled") ;
       // 3. Add the amount to the user's balance. Hint: the amount can be
       //    accessed from of the global variable `msg`
-
+        balances[msg.sender] += msg.value;
       // 4. Emit the appropriate event associated with this function
-
+        emit LogDepositMade(msg.sender, msg.value);
       // 5. return the balance of sndr of this transaction
+        return balances[msg.sender];
     }
 
     /// @notice Withdraw ether from bank
@@ -91,11 +95,19 @@ contract SimpleBank {
       // to the user attempting to withdraw. 
       // return the user's balance.
 
+
       // 1. Use a require expression to guard/ensure sender has enough funds
 
+      require(balances[msg.sender]>= withdrawAmount, "Insufficient Balance");
+      
       // 2. Transfer Eth to the sender and decrement the withdrawal amount from
       //    sender's balance
 
+      balances[msg.sender] -= withdrawAmount;
+      msg.sender.transfer(withdrawAmount);
+
       // 3. Emit the appropriate event for this message
+      emit LogWithdrawal(msg.sender, withdrawAmount, balances[msg.sender]);
+      return balances[msg.sender];
     }
 }
